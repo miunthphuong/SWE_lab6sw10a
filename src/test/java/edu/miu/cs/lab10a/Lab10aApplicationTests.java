51e25e4773d65f5eb6481e6afdfe7053fd470711
Phuong Nguyen <miu.nthphuong@gmail.com>
2026-06-09 15:27:23 -0500

    spring source code

--- src/test/java/edu/miu/cs/lab10a/Lab10aApplicationTests.java
+++ src/test/java/edu/miu/cs/lab10a/Lab10aApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Lab10aApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
